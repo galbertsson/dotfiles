706fa307c70183c1714e1a10cbc1816f7797f8cb
galbertsson <42936045+galbertsson@users.noreply.github.com>
2023-11-25 23:06:46 +0100

    More keybinds and rgrep

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("n", "K", ":m '<-2<CR>gv=gv")
+
+-- System clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>p", '"+p')
